ab55b3086710aebc86b17275de39737bb61e24cd
KatlheenEscoto <escotokatlheen@gmail.com>
2020-08-26 17:46:06 -0600

    Commit 4 | Migracion exitosa.

---
+++ CI.DAL/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+using CI.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CI.DAL
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+        { }
+
+        public DbSet<Value> Values { get; set; }
+
+    }
+}
